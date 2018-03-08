a9954726baf7a562982962e16fc90a1b9efbe506
bbarbs <barberobrainold@gmail.com>
2018-03-08 10:43:55 +0800

    Added stubbing on test case to print the sample result on Async

--- src/test/java/com/demo/web/CustomerControllerTest.java
+++ src/test/java/com/demo/web/CustomerControllerTest.java
@@ -3,8 +3,10 @@ package com.demo.web;
 import com.demo.model.Customer;
 import com.demo.service.impl.CustomerServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.reactivex.Maybe;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,11 +14,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -31,6 +36,9 @@ public class CustomerControllerTest {
 
     @Test
     public void testShouldGetAllCustomers() throws Exception {
+        Customer customer = new Customer();
+        customer.setEmail("test@gmail.com");
+        when(this.service.findAllCustomer()).thenReturn(Maybe.just(Arrays.asList(customer)));
         this.mockMvc.perform(
                 get("/customers")
                         .contentType(APPLICATION_JSON_VALUE)
@@ -48,6 +56,7 @@ public class CustomerControllerTest {
         customer.setFirstname("Test");
         ObjectMapper mapper = new ObjectMapper();
         String body = mapper.writeValueAsString(customer);
+        when(this.service.addCustomer(Mockito.any(Customer.class))).thenReturn(Maybe.just(customer));
         this.mockMvc.perform(
                 post("/customers")
                         .accept(APPLICATION_JSON_VALUE)
